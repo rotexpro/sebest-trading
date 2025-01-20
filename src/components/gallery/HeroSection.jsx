6b615a918d9370cd5e060fed81471fdc2e5b9f18
Rotimi Abayomi <rotimiabayomi123@gmail.com>
2025-01-20 15:23:21 +0100

    Update HeroSection.jsx

--- src/components/gallery/HeroSection.jsx
+++ src/components/gallery/HeroSection.jsx
@@ -4,13 +4,13 @@ import HeroSectionImage from "../../assets/images/HeroSectionImage.png";
 const HeroSection = () => {
   return (
     <div
-      className="relative bg-cover bg-center h-[300px] flex items-center"
+      className="relative mt-[10vh] bg-cover bg-center h-[300px] flex items-center"
       style={{
         backgroundImage: `url(${HeroSectionImage})`,
       }}
     >
       <div className="absolute inset-0  bg-[#1E3F62] opacity-60"></div>
-      <h1 className="relative px-6 md:px-12 text-[#F3F3F3] text-3xl lg:text-5xl font-bold px-4">
+      <h1 className="relative container mx-auto px-4 text-[#F3F3F3] text-3xl lg:text-5xl font-bold px-4">
         GALLERY
       </h1>
     </div>
