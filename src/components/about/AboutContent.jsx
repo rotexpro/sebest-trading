12acc66d6a7d665738c0e848393413cb843bed8b
Rotimi Abayomi <rotimiabayomi123@gmail.com>
2025-01-20 14:53:22 +0100

    Update AboutContent.jsx
    Replaced about us

--- src/components/about/AboutContent.jsx
+++ src/components/about/AboutContent.jsx
@@ -4,14 +4,11 @@ const AboutContent = () => {
   return (
     <div className="container mx-auto px-6 py-8 md:py-12">
       <p className="text-gray-700 text-xs md:text-sm lg:text-lg leading-[16.94px] md:leading-[19.36px]">
-        At Sebest, we are passionate about providing high-quality frozen fish to
-        retailers across the region. A a trusted leader in the seafood wholesale
-        industry, we pride ourselves on sourcing sustainably caught fish that
-        has been flat-frozen to preserve freshness, and ensuring it reaches you
-        in peak condition. With state-of-the-art warehouses, we maintain the
-        highest standards of storage and delivery. From trusted suppliers to
-        retailers nationwide, our network ensures the prompt and efficient
-        delivery of high-quality processed and frozen fish.
+        We are a business that deals with sourcing and supplying diverse, nutritious frozen food products.
+        Identifying the challenges posed by a growing population and the limitations of local farming and 
+        supply chains, we actively bridge the gap by sourcing essentials across borders. 
+        Through strategic partnerships and efficient supply networks, we ensure a steady flow 
+        of high-quality food products to meet surging demands.
       </p>
     </div>
   );
